27d8e4c78eed6f5109c40f5a7988ffc1ce7d370c
stas-shr <stasshr@gmail.com>
2023-05-21 16:14:07 +0300

    Stretching picture done

--- Stretching Picture/ViewController.swift
+++ Stretching Picture/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    let pictureHeight: CGFloat = 270
+    
+    lazy var picture: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "Cat")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.frame.origin = .zero
+        imageView.frame.size = CGSize(width: view.frame.width, height: pictureHeight)
+        return imageView
+    }()
+    
+    lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.insetsLayoutMarginsFromSafeArea = false
+        scrollView.contentInsetAdjustmentBehavior = .never
+        scrollView.delegate = self
+        scrollView.frame = view.frame
+        scrollView.contentSize = CGSize(width: view.frame.width, height: 1500)
+        scrollView.verticalScrollIndicatorInsets.top = pictureHeight - view.safeAreaInsets.top
+        return scrollView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        view.addSubview(scrollView)
+        scrollView.addSubview(picture)
     }
-
-
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView.contentOffset.y < 0 {
+            let imageViewHeight = pictureHeight - scrollView.contentOffset.y
+            
+            picture.frame.origin = CGPoint(x: 0, y: scrollView.contentOffset.y)
+            picture.frame.size = CGSize(width: view.frame.width, height: imageViewHeight)
+            scrollView.verticalScrollIndicatorInsets.top = imageViewHeight - view.safeAreaInsets.top
+        }
+    }
 }
-
